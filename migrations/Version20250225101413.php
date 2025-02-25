6bfed1bddfc0d0cbfd9043dbce8bcd67562cae20
julfranc44 <julfranc44@gmail.com>
2025-02-25 14:07:07 +0100

    changement migration

--- migrations/Version20250225101413.php
+++ migrations/Version20250225101413.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250224153716 extends AbstractMigration
+final class Version20250225101413 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -24,6 +24,7 @@ final class Version20250224153716 extends AbstractMigration
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE location (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, roadnumber VARCHAR(5) DEFAULT NULL, roadname VARCHAR(255) DEFAULT NULL, zipcode VARCHAR(5) NOT NULL, townname VARCHAR(255) NOT NULL, longitude BIGINT NOT NULL, latitude BIGINT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE status (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
@@ -33,5 +34,6 @@ final class Version20250224153716 extends AbstractMigration
         $this->addSql('DROP TABLE category');
         $this->addSql('DROP TABLE location');
         $this->addSql('DROP TABLE status');
+        $this->addSql('ALTER TABLE `user` DROP is_verified');
     }
 }
