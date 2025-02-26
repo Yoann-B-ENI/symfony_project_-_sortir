7fe4d5cd14b85fb9a260719147a2c118d5f7c327
AmélieC <185417630+AmeLIE2024@users.noreply.github.com>
2025-02-26 09:28:08 +0100

    Page de connexion, déconnexion correction

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,7 +8,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\Validator\Constraints\PasswordStrength;
+
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
