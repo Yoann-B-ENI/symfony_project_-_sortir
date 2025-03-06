5078c57ee8084e580b190dd1f17f0d11d6e743ee
Yoann Battu <yoann.battu2024@campus-eni.fr>
2025-03-06 10:44:15 +0100

    add fixtures

--- src/DataFixtures/LocationFixtures.php
+++ src/DataFixtures/LocationFixtures.php
@@ -16,7 +16,7 @@ class LocationFixtures extends Fixture
          * START OF LOCATION LIST
          */
         $temp = new Location();
-        $temp->setName("petit 5 guys d'atlantis :)")
+        $temp->setName("Five Guys d'Atlantis :)")
             ->setRoadname("Boulevard Salvador Allende")
             ->setZipcode("44880")
             ->setTownname("Saint-Herblain")
@@ -28,7 +28,7 @@ class LocationFixtures extends Fixture
         $this->addReference('location_1', $temp);
 
         $temp = new Location();
-        $temp->setName("ptit KFC bien sympa")
+        $temp->setName("KFC d'Atlantis bien sympa")
             ->setRoadnumber(6)
             ->setRoadname("Rue des Cochardières")
             ->setZipcode("44880")
@@ -49,6 +49,57 @@ class LocationFixtures extends Fixture
         ;
         $manager->persist($temp);
         $this->addReference('location_3', $temp);
+
+        $temp = new Location();
+        $temp->setName("Jack's Corner")
+            ->setRoadnumber(2)
+            ->setRoadname("Rue de la Baclerie")
+            ->setZipcode("44000")
+            ->setTownname("Nantes")
+            ->setLongitude(-1.5531639766224583)
+            ->setLatitude(47.215180387139704)
+        ;
+        $manager->persist($temp);
+        $this->addReference('location_4', $temp);
+
+        $temp = new Location();
+        $temp->setName("Château des ducs de Bretagne - expo")
+            ->setRoadnumber(4)
+            ->setRoadname("Place Marc Elder")
+            ->setZipcode("44000")
+            ->setTownname("Nantes")
+            ->setLongitude(-1.5489023392602903)
+            ->setLatitude(47.21664770980089)
+            ->setExtraInfo("Bâtiment de gauche")
+        ;
+        $manager->persist($temp);
+        $this->addReference('location_5', $temp);
+
+        $temp = new Location();
+        $temp->setName("Pathé Atlantis")
+            ->setRoadnumber(8)
+            ->setRoadname("Allée la Pérouse")
+            ->setZipcode("44800")
+            ->setTownname("Saint-Herblain")
+            ->setLongitude(-1.6287900320832804)
+            ->setLatitude(47.22284871484565)
+        ;
+        $manager->persist($temp);
+        $this->addReference('location_6', $temp);
+
+
+        $temp = new Location();
+        $temp->setName("Parc de Procé - Manoir")
+            ->setRoadnumber(44)
+            ->setRoadname("Rue des Dervallières")
+            ->setZipcode("44000")
+            ->setTownname("Nantes")
+            ->setLongitude(-1.582131414650181)
+            ->setLatitude(47.22413881986674)
+            ->setExtraInfo("suivre indications manoir une fois dedans")
+        ;
+        $manager->persist($temp);
+        $this->addReference('location_7', $temp);
         /*
          * END OF LOCATION LIST
          */
